0bc075b7f0fcfe301441a23f4e364b8fc9270f60
laffeymyth <java51300@gmail.com>
2025-03-08 11:53:30 -0800

    BoardFactory fix

--- src/main/java/net/laffeymyth/fastboard/pretty/BoardFactory.java
+++ src/main/java/net/laffeymyth/fastboard/pretty/BoardFactory.java
@@ -4,12 +4,14 @@ import fr.mrmicky.fastboard.FastBoard;
 import fr.mrmicky.fastboard.FastBoardBase;
 import org.bukkit.entity.Player;
 
+import java.awt.*;
+
 class BoardFactory {
     @SuppressWarnings("unchecked")
     public static <T> FastBoardBase<T> createFastBoardBase(Player player, Class<T> boardClass) {
-        if (boardClass == FastBoard.class) {
+        if (boardClass == String.class) {
             return (FastBoardBase<T>) new FastBoard(player);
-        } else if (boardClass == fr.mrmicky.fastboard.adventure.FastBoard.class) {
+        } else if (boardClass == Component.class) {
             return (FastBoardBase<T>) new fr.mrmicky.fastboard.adventure.FastBoard(player);
         } else {
             throw new IllegalArgumentException("Unsupported type: " + boardClass.getName());
